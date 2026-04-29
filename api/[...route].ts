501e791ca841f59475aaec190f515f84d5b85c28
Molly <doris_789@126.com>
2026-04-29 20:07:53 +0800

    Wrap Vercel API entry as node handler

--- api/[...route].ts
+++ api/[...route].ts
@@ -1,5 +1,12 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
 import { createApp } from "../app.ts";
 
 const app = createApp();
 
-export default app;
+export const config = {
+  runtime: "nodejs",
+};
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  return app(req, res);
+}
